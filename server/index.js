2c2bbeaa78820b7bb341384e94d8750a6beb2e20
PROPHESSOR <xenteze@gmail.com>
2018-04-15 22:28:30 +0300

    Easy to switch port `node index.js port`

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const WebSocket = require("ws");
 const browserOpen = require("../utils/openBrowser");
 
-let port, backend;
+let backend;
 
 
 function handleWebSocket(ws) {
@@ -11,14 +11,19 @@ function handleWebSocket(ws) {
 	backend.send = msg => ws.send(JSON.stringify(msg));
 }
 
+// Choose random port, so that noone can steal local files from Web
+function getPort() {
+	return Number(process.argv[2]) || Math.floor(Math.random() * 3000) + 2000;
+}
+
+
 module.exports = b => {
+	const port = getPort()
 	backend = b;
 
-	// Choose random port, so that noone can steal local files from Web
-	port = Math.floor(Math.random() * 3000) + 2000;
 	console.log("Listening on port " + port);
 
 	browserOpen(`http://localhost:${port}`);
 
-	new WebSocket.Server({port: port}).on("connection", handleWebSocket);
+	new WebSocket.Server({port}).on("connection", handleWebSocket);
 }
