c664fd7f957c5bddb7caf3185a07df0ff3958b7d
Ivanq <imachug@yandex.ru>
2018-04-15 17:14:33 +0300

    Use websocket

--- gui/js/main.js
+++ gui/js/main.js
@@ -1,7 +1,19 @@
 // TODO: don't get port via prompt
 const port = parseInt(prompt("HTTP port:"));
+let ws = null;
 
 function get(url) {
 	return fetch(`http://localhost:${port}/${url}`)
 		.then(res => res.text());
+}
+
+function connect() {
+	return get("_port")
+		.then(wsPort => {
+			ws = new WebSocket(`ws://localhost:${wsPort}`);
+		});
+}
+
+function sendWs(msg) {
+	ws.send(msg);
 }
